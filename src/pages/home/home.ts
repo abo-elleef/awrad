27ea318dfbd0985aaafc425d56cdd64dc573aaa2
leef <a.aboelllef@gmail.com>
2017-11-16 14:00:27 +0200

    add basic awrad and basic grid to view theme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Awrad } from '../../app/awrad';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  awrad = [] ;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController
+  ) {
+    this.awrad = Awrad
 
+  }
+  ionViewWillEnter() {
+    // this._nativeStorage.getItem('fontSize').then(data =>{
+    //   this.fontSize = data ?  data : this.fontSize;
+    // });
+    // this._nativeStorage.getItem('fontFace').then(data => {
+    //   this.fontFaceClass = data ? data: this.fontFaceClass
+    // });
   }
 
 }
